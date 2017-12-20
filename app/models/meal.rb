671a9f724abcd56f3b79e262528c386509ad4fb4
OswaldoHenrique <oswaldohenriquem@gmail.com>
2017-12-19 22:28:56 -0200

    Arruma model e routes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :meal_category
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :available, inclusion: { in: [false, true] }
+  validates :available, exclusion: { in: [nil] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :meal_categories
+  resources :meal_categories do
+    resources :meals
+  end
 
   root 'home#index'
 end
